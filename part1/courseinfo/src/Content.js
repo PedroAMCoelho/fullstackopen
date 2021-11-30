3debbadb5aaa4051f701c7242b39a54ba23f9838
PedroAMCoelho <pedrocoelhoprojetos@gmail.com>
2021-11-30 23:42:14 +0000

    part 2 courseinfo step 8 - was already done in step 7

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Part from './Part';
 
 const Content = ({parts}) => {
-    return (parts.map(p => <Part key={p.id} part={p} />))
+    return parts.map(p => <Part key={p.id} part={p} />)
   }
 
   export default Content;
