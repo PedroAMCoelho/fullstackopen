c495ecf259148802d37c1107fd17c63475db2453
PedroAMCoelho <pedrocoelhoprojetos@gmail.com>
2021-11-30 23:18:07 +0000

    part 2 courseinfo step 7

--- part1/courseinfo/src/Total.js
+++ part1/courseinfo/src/Total.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const Total = ({total}) => {
-    return (
-      <>
-        <p>Number of exercises {total[0].exercises + total[1].exercises + total[2].exercises}</p>
-      </>
-    )
+    return (<b>total of {total.reduce((acc, curr) => acc + curr.exercises, 0)} exercises</b>)
   }
 
   export default Total;
