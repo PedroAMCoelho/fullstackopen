b73344a1b13ccc5467f6c32fb3186336bacc94c7
PedroAMCoelho <pedrocoelhoprojetos@gmail.com>
2021-12-05 01:01:27 +0000

    part2 phonebook steps 6 and 7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import PersonsDataTable from './components/PersonsDataTable';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newPhoneNumber, setNewPhoneNumber] = useState(null);
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3002/persons')
+      .then(response => {
+        setPersons(response.data);
+      })
+  }, [])
 
   const handleFilterChange = (event) => setFilter(event.target.value.toLowerCase());
 
@@ -22,10 +26,17 @@ const App = () => {
 
   const addPerson = (event) => {
     if (isValidNewPerson(newName))
-      setPersons([
-        ...persons,
-        { id: persons.length + 1, name: newName, number: newPhoneNumber },
-      ]);
+      {
+        let newPerson = { name: newName, number: newPhoneNumber };
+
+        axios
+        .post('http://localhost:3002/persons', newPerson)
+        .then(response => {
+          setPersons(persons.concat(response.data));
+          setNewName('');
+          setNewPhoneNumber('');
+        });
+      }
     else invalidPersonAlert(newName);
   };
 
